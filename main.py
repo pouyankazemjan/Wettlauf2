e4f35762aaf55d6e4ff089a28c72e776121b88b9
Pouyan_Kazemjan <99509787+pouyankazemjan@users.noreply.github.com>
2022-11-17 17:24:46 +0100

    WettLauf2
    Der Igel m¨ochte in einem Wettlauf gegen den Hasen antreten. Naturgem¨aß hat der Igel eine
    kurzere Schrittl ¨ ¨ange als der Hase: der Igel hat eine Schrittl¨ange von 5.3 cm, der Hase hat eine
    Schrittl¨ange von 10.8 cm. Um diesen Vorteil des Hasens auszugleichen, bekommt der Igel einen
    Vorsprung von 270 cm
    nach wie vielen Schritten der Hase den Igel trotz
    des Vorsprungs uberholt hat.

---
+++ main.py
@@ -0,0 +1,29 @@
+Schrietehase =  10
+Schriteigel =  4.3
+Vorsprung = 200
+streckehase= 0
+Streckeigel= Vorsprung
+
+Schritte= 0
+
+while Streckeigel > Streckehase:
+    Streckehase = Streckehase + Schrietehase
+    Streckeigel = Streckeigel + Schriteigel
+    Schritte+=1
+
+
+    print("nach" + str(Schritte) + " Streckehase=" + str(Streckehase)+"km Streckeigel =" + str(Streckeigel)+"km")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
